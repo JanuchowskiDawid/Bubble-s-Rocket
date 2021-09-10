66de765bd8fa0214dbaba2dd9005bee180879428
mmalpa <mmalpa97@wp.pl>
2021-09-10 21:24:23 +0200

    Created simple mechanism of flying

---
+++ Assets/Flying.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    Rigidbody rigidbdy;
+    [SerializeField] float acceleration = 100f;
+    [SerializeField] float rotationSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbdy = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fly();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rotation(rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Rotation(-rotationSpeed);
+        }
+    }
+
+    private void Rotation(float rotationThisFrame)
+    {
+        transform.Rotate(0, 0, rotationThisFrame * Time.deltaTime);
+    }
+
+    private void Fly()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbdy.AddRelativeForce(0, acceleration*Time.deltaTime, 0);
+        }
+    }
+}
