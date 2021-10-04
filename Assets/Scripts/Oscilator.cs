858e1e148a0eecf68a181049ed3a799e43f19812
mmalpa <mmalpa97@wp.pl>
2021-10-04 22:07:05 +0200

    Changed fuction of moving obstacles, now it uses sin function

--- Assets/Scripts/Oscilator.cs
+++ Assets/Scripts/Oscilator.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Oscilator : MonoBehaviour
 {
+    const float tau = Mathf.PI * 2;
+    [SerializeField] float period = 2f;
+    float cycles;
+    float rawSinWave;
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)]float movementFactor;
+    
+    [SerializeField] [Range(0,1)] float movementFactor;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +21,15 @@ public class Oscilator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period<=Mathf.Epsilon)
+        {
+            return;
+        }
+        cycles = Time.time / period;
+        rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSinWave + 1f) / 2f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
+
     }
 }
